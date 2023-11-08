004f48bdb842b138f3a84e7093864fcf48c3b528
MikkelFJ <mikkel@dvide.com>
2023-11-08 14:45:25 +0100

    Create dir if it not present when linking files

--- .config/bin/__link_file.sh
+++ .config/bin/__link_file.sh
@@ -9,11 +9,16 @@
 
 SRC=$1/$2
 DST=$HOME/$2
+DST_DIR=$(dirname $DST)
 if [ -e $SRC ] ; then
     # if not a symbolic link, back it up
     if [ -e $DST ] && [ ! -L $DST ] ; then
         echo "  backing up '$DST' to '$DST.backup'"
         mv $DST $DST.backup
+    fi
+    if [ ! -d "$DST_DIR" ]; then
+        echo "  creating directory '$DST_DIR'"
+        mkdir -p $DST_DIR
     fi
     echo "  linking $DST"
     ln -sf $SRC $DST
